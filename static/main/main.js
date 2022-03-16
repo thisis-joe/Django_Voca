94729e151cc2d007ad52083772f12e7b9fd27815
joseph <wdg0434@gmail.com>
2022-03-16 17:30:14 +0900

    js test8

--- static/main/main.js
+++ static/main/main.js
@@ -1,4 +1,4 @@
-const button1 = document.querySelector('#button1');
+const button1 = document.querySelector('.button1');
 
 button1.addEventListener('click', () => {
     document.getElementById("button1").innerHTML = Date();
